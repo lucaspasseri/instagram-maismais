a0c845b1ed06472e69ff4552b72a1833f9cce797
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-04-29 22:46:29 -0300

    Adição do projeto pronto mais componentização do mesmo.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import Corpo from './Corpo';
+import FundoMobile from './FundoMobile';
+
+export default function App(){
+    return(
+        <>
+            <Navbar />
+            <Corpo />
+            <FundoMobile />
+        </>
+    );
+}
